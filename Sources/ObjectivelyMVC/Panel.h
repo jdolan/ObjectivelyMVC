06393f8fc63c44162ea5f329060888e5c50c336b
Jay Dolan <jay@jaydolan.com>
2016-09-12 21:18:38 -0400

    Increase padding slightly on Panel. This makes Box labels easier on the eyes.

--- Sources/ObjectivelyMVC/Panel.h
+++ Sources/ObjectivelyMVC/Panel.h
@@ -33,9 +33,9 @@
  * @brief Draggable containers.
  */
 
-#define DEFAULT_PANEL_PADDING 10
+#define DEFAULT_PANEL_PADDING 12
 #define DEFAULT_PANEL_RESIZE_HANDLE_SIZE 10
-#define DEFAULT_PANEL_SPACING 10
+#define DEFAULT_PANEL_SPACING 12
 
 typedef struct Panel Panel;
 typedef struct PanelInterface PanelInterface;
