a72b649e53ca73fb51f93fc3d1fbbd156e229473
Paril <paril@alteredsoftworks.com>
2016-10-25 11:59:33 -0400

    SDL 2.0.5 for Winders, and use the .lib/.dll from the zip instead of storing in repo

--- ObjectivelyMVC.vs15/libs/sdl/SDL2/PREPARE.ps1
+++ ObjectivelyMVC.vs15/libs/sdl/SDL2/PREPARE.ps1
@@ -1,7 +1,9 @@
 If (!(Test-Path tmp.zip)) {
     Write-Output "Downloading SDL..."
-    (New-Object System.Net.WebClient).DownloadFile("https://www.libsdl.org/release/SDL2-devel-2.0.4-VC.zip", "tmp.zip")
+    (New-Object System.Net.WebClient).DownloadFile("https://www.libsdl.org/release/SDL2-devel-2.0.5-VC.zip", "tmp.zip")
 }
 
 Write-Output "Extracting SDL..."
-7z e tmp.zip -aos "SDL2-2.0.4\include\*.h"
+7z e tmp.zip -aos "SDL2-2.0.5\include\*.h"
+7z e tmp.zip -aos "SDL2-2.0.5\lib\x86\SDL2.*" -o"..\lib\Win32\"
+7z e tmp.zip -aos "SDL2-2.0.5\lib\x64\SDL2.*" -o"..\lib\x64\"
