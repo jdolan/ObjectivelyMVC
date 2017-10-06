4ecabe5ebd947384f961c622cba1efff8456645d
Jay Dolan <jay@jaydolan.com>
2017-10-05 21:00:05 -0400

    Only delete a texture if we have one. Might solve some r_restart issues in Quetoo.

--- Sources/ObjectivelyMVC/Text.c
+++ Sources/ObjectivelyMVC/Text.c
@@ -40,7 +40,9 @@ static void dealloc(Object *self) {
 
 	free(this->text);
 
-	glDeleteTextures(1, &this->texture);
+	if (this->texture) {
+		glDeleteTextures(1, &this->texture);
+	}
 
 	super(Object, self, dealloc);
 }
