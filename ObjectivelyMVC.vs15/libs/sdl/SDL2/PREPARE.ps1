1aadae645be8a60ff3681e918ddac74ffcaa16f4
Paril <paril@alteredsoftworks.com>
2017-10-26 17:36:23 -0400

    Update SDL and SDL_image to latest. They fixed cpuinfo, yay.

--- ObjectivelyMVC.vs15/libs/sdl/SDL2/PREPARE.ps1
+++ ObjectivelyMVC.vs15/libs/sdl/SDL2/PREPARE.ps1
@@ -1,9 +1,9 @@
-If (!(Test-Path tmp.zip)) {
+If (!(Test-Path tmp1.zip)) {
     Write-Output "Downloading SDL..."
-    (New-Object System.Net.WebClient).DownloadFile("https://www.libsdl.org/release/SDL2-devel-2.0.5-VC.zip", "tmp.zip")
+    (New-Object System.Net.WebClient).DownloadFile("https://www.libsdl.org/release/SDL2-devel-2.0.7-VC.zip", "tmp1.zip")
 }
 
 Write-Output "Extracting SDL..."
-7z e tmp.zip -aos "SDL2-2.0.5\include\*.h"
-7z e tmp.zip -aos "SDL2-2.0.5\lib\x86\SDL2.*" -o"..\lib\Win32\"
-7z e tmp.zip -aos "SDL2-2.0.5\lib\x64\SDL2.*" -o"..\lib\x64\"
+7z e tmp1.zip -aoa "SDL2-2.0.7\include\*.h"
+7z e tmp1.zip -aoa "SDL2-2.0.7\lib\x86\SDL2.*" -o"..\lib\Win32\"
+7z e tmp1.zip -aoa "SDL2-2.0.7\lib\x64\SDL2.*" -o"..\lib\x64\"
--- ObjectivelyMVC.vs15/libs/sdl_image/SDL2/PREPARE.ps1
+++ ObjectivelyMVC.vs15/libs/sdl_image/SDL2/PREPARE.ps1
@@ -1,9 +1,9 @@
-If (!(Test-Path tmp.zip)) {
+If (!(Test-Path tmp1.zip)) {
     Write-Output "Downloading SDL_image..."
-    (New-Object System.Net.WebClient).DownloadFile("https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.1-VC.zip", "tmp.zip")
+    (New-Object System.Net.WebClient).DownloadFile("https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.2-VC.zip", "tmp1.zip")
 }
 
 Write-Output "Extracting SDL_image..."
-7z e tmp.zip -aos "SDL2_image-2.0.1\include\*.h"
-7z e tmp.zip -aos "SDL2_image-2.0.1\lib\x86\*" -o"..\lib\Win32\"
-7z e tmp.zip -aos "SDL2_image-2.0.1\lib\x64\*" -o"..\lib\x64\"
+7z e tmp.zip -aoa "SDL2_image-2.0.2\include\*.h"
+7z e tmp.zip -aoa "SDL2_image-2.0.2\lib\x86\*" -o"..\lib\Win32\"
+7z e tmp.zip -aoa "SDL2_image-2.0.2\lib\x64\*" -o"..\lib\x64\"
